8738fd658a1d74ac3775393ce9308c29c97c7fcd
aprianmulyana <developereciid@gmail.com>
2025-11-18 11:55:08 +0700

    update kotlin version

--- android/src/main/kotlin/com/mesosfer/flimer/FlimerPlugin.kt
+++ android/src/main/kotlin/com/mesosfer/flimer/FlimerPlugin.kt
@@ -7,14 +7,9 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlimerPlugin */
 class FlimerPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
